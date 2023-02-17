0cea5603aeed3c2d9fa3635b69d6984b48abb1eb
Mathijs <MathijsM2006@gmail.com>
2023-02-17 15:07:22 +0100

    herugoewheohijigrgiigijoijgo

--- src/swapchain.rs
+++ src/swapchain.rs
@@ -1,4 +1,4 @@
-use vulkanalia::{prelude::v1_0::*, vk::{SwapchainKHR, SwapchainCreateInfoKHR}};
+use vulkanalia::{prelude::v1_0::*, vk::{SwapchainKHR, SwapchainCreateInfoKHR, KhrSurfaceExtension}};
 use anyhow::Result;
 
 use crate::app::AppData;
@@ -17,15 +17,18 @@ pub unsafe fn create_swapchain(data: &AppData) -> Result<SwapchainKHR> {
 
 
 
-struct SwapchainSupports {
+struct SwapchainSupport {
     capabilities: vk::SurfaceCapabilitiesKHR,
     formats: Vec<vk::SurfaceFormatKHR>,
     present_modes: Vec<vk::PresentModeKHR>
 }
 
 
-impl  SwapchainSupports {
+impl  SwapchainSupport {
     unsafe fn get(instance: &Instance, data: &AppData, physical_device: &vk::PhysicalDevice) -> Result<Self> {
-        
+        let formats = instance.get_physical_device_surface_formats_khr(*physical_device, data.surface)?;
+        let capabilities = instance.get_physical_device_surface_capabilities_khr(*physical_device, data.surface)?;
+        let present_modes = instance.get_physical_device_surface_present_modes_khr(*physical_device, data.surface)?;
+        return Ok(Self {capabilities, formats, present_modes});
     }
 }
